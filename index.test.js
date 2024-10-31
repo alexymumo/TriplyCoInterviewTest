1e1c576f341de7699bac64f53609921851356e4f
Alex Mumo <alexymumo2000@gmail.com>
2024-10-31 08:12:23 +0300

    Add bookings tests

--- index.test.js
+++ index.test.js
@@ -1,8 +1,10 @@
 const request = require('supertest');
 
+const BASE_URL = 'https://restful-booker.herokuapp.com';
+
 describe('POST /auth', () => {
   it('should return a valid token when correct credentials are provided', async () => {
-    const response = await request('https://restful-booker.herokuapp.com')
+    const response = await request(BASE_URL)
       .post('/auth')
       .set('Content-Type', 'application/json')
       .send({
@@ -11,11 +13,11 @@ describe('POST /auth', () => {
       });
 
     expect(response.statusCode).toBe(200); 
-    expect(response.body).toHaveProperty('token'); // Check if the token is present
+    expect(response.body).toHaveProperty('token'); 
   });
 
   it('should return Bad credential for invalid credentials provided', async () => {
-    const response = await request('https://restful-booker.herokuapp.com')
+    const response = await request(BASE_URL)
       .post('/auth')
       .set('Content-Type', 'application/json')
       .send({
@@ -26,5 +28,26 @@ describe('POST /auth', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body).toHaveProperty('reason')
   });
+});
+
+describe('GET /booking', () =>{
+  it('should return all the booking ids in the api', async () =>{
+    const res = await request(BASE_URL)
+    .get('/booking')
+    .send();
+    expect(res.statusCode).toBe(200);
+  });
+  it('should return bookings by firstname and lastname', async () => {
+    const res = await request(BASE_URL)
+    .get('/booking')
+    .query({firstname:'sally',lastname:'brown'})
+    .send();
+    expect(res.statusCode).toBe(200);
+
+  });
+
+  it('should return bookings by checkin/checkout date',async () => {
+
+  })
 });
 
